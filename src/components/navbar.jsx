40419ce3ad60b7f37702a0f9de1a3dad76e8ca32
Mugisha8 <yvesmugisha8@gmail.com>
2023-10-16 06:09:37 -1000

    components upgrade

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,37 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
+import { IconContext } from "react-icons";
 import { Link } from "react-router-dom";
+import { FaBars } from "react-icons/fa";
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
-    <>
-      <div className="menus">
-        <Link to="/">
-          {" "}
-          <h2>M<span class="logo">eight</span> Blogs</h2>
-        </Link>
+    <IconContext.Provider value={{ color: "white", size: "2em" }}>
+      <>
+        <div className="menus">
+          <Link to="/">
+            {" "}
+            <h2>
+              M<span class="logo">eight</span> Blogs
+            </h2>
+          </Link>
+          <ul className={menuOpen ? "open" : ""}>
+            <Link to="/" className="text">
+              Home
+            </Link>
 
-        <ul>
-          <Link to="/" className="text">
-            Home
+            <Link to="/blogpost" className="text">
+              Blog Posts
+            </Link>
+
+            <Link to="/About" className="text">
+              About
+            </Link>
+
+            <Link to="/contact" className="text">
+              Contact
+            </Link>
+          </ul>
+          <Link to="/signin">
+            <button class="dash_signin">SIGN IN</button>
           </Link>
 
-          <Link to="/blogpost" className="text">
-            Blog Posts
-          </Link>
-
-          <Link to="/About" className="text">
-            About
-          </Link>
-
-          <Link to="/contact" className="text">
-            Contact
-          </Link>
-        </ul>
-        <Link to="/signin">
-          <button class="dash_signin">SIGN IN</button>
-        </Link>
-      </div>
-    </>
+          <div
+            className="MobileMenu"
+            onClick={() => {
+              setMenuOpen(!menuOpen);
+            }}
+          >
+            <FaBars />
+          </div>
+        </div>
+      </>
+    </IconContext.Provider>
   );
 };
 
